206f275bcc04bd6b446b5d99c9b93ed321cd6d4b
adaseleticia <adaseleticia@gmail.com>
2019-10-16 16:16:20 +0900

    assignment 3
    simple webpage

---
+++ 3_Design_simple_webpage/js/wk.js
@@ -0,0 +1,8 @@
+function leti() {
+			alert('hello, I am very excited to start learning how to code.Thank DiV Team for the opportunity. Just started JS lessons');
+		}
+
+window.onload = function()
+{
+	leti();
+};
